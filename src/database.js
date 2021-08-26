791bb0001e08e551e794fb0644d1b2fc8aac902c
Mr.Vee <m@MOMA-PC.localdomain>
2021-08-26 16:10:57 +0100

    New feature! Added prisma

--- src/database.js
+++ src/database.js
@@ -1,31 +1,12 @@
 
 
 // node-graphql/src/database.js
+//* node-graphql/src/database.js
 
-const students = [
-    {
-      id: 1,
-      email: 'ada@telixia.com',
-      fullName: 'Ada Eze',
-      dept: 'Software Engineering',
-      enrolled: true,
-    },
-    {
-      id: 2,
-      email: 'musa@telixia.com',
-      fullName: 'Musa Bashir',
-      dept: 'Data Engineering',
-      enrolled: true,
-    },
-    {
-      id: 3,
-      email: 'olusegun@obj.com',
-      fullName: 'Obasanjo Omajo',
-      dept: 'System hacking and destruction',
-      enrolled: true,
-    },
-  ]
+const { PrismaClient } = require('@prisma/client')
 
-  module.exports ={
-      students
-  }
+const prisma = new PrismaClient();
+
+module.exports = {
+  prisma,
+}
